5455621ded229a96589579e4e780e5e68c207e04
Elisey <Jiraiya106@gmail.com>
2021-08-16 18:59:30 +0300

    Linux 2.3

--- Linux/First_task/copy-dir.sh
+++ Linux/First_task/copy-dir.sh
@@ -3,60 +3,61 @@
 SOURCE_DIR=$(realpath "$1")
 DESTINATION_DIR=$(realpath "$2")
 
-echo $SOURCE_DIR $DESTINATION_DIR
-
-if [ "$SOURCE_DIR" == "$DESTINATION_DIR" ]
+idenname () {
+if [ "$1" == "$2" ]
 then
     echo "ERROR 1"
     #exit 0
 fi
+}
 
 #Свободное место на диске
 freedisk () {
-    FREEDISK=$(df "$1" | tail -1 | awk '{print$4}')
+    df "$1" | tail -1 | awk '{print$4}'
 }
 
 parentdir () {
-    PARENT_DIR=$(dirname $1)
+    dirname $1
 }
 
-parentdir $SOURCE_DIR
-PARENT_SOURCE=$PARENT_DIR
-parentdir $DESTINATION_DIR
-PARENT_DEST=$PARENT_DIR
+directoryproblem () {
+PARENT_SOURCE=$(parentdir $SOURCE_DIR)
+PARENT_DESTINATION=$(parentdir $DESTINATION_DIR)
 
-echo $PARENT_SOURCE $PARENT_DEST
-
-# if [ "$SOURCE_DIR" == "$PARENT_DEST" ]
-# then
-#     echo "ERROR 2"
-#     #exit 0
-# elif [ "$DESTINATION_DIR" == "$PARENT_SOURCE" ]
-# then 
-#     echo "ERROR 3"
-# fi
+if [ "$SOURCE_DIR" == "$PARENT_DESTINATION" ]
+then
+    echo ""$DESTINATION_DIR" parent directory "$SOURCE_DIR""
+    exit 0
+elif [ "$DESTINATION_DIR" == "$PARENT_SOURCE" ]
+then 
+    echo ""$SOURCE_DIR" parent directory "$DESTINATION_DIR""
+fi
+}
 
  #Объем папки
-DIRSPACE=$(du -s "$SOURCE_DIR" | awk '{print$1}')
+dirspace () {
+    du -s "$1" | awk '{print$1}'
+}
 
-freedisk $DESTINATION_DIR
-
-#Скопировать папки
+#Copy direct
 copyall () {
     cp -p -r $1 $2
 }
 
 copyfile () {
-if [ "$DIRSPACE" -lt "$FREEDISK" ]
+FREE_DISK_DEST=$(freedisk $DESTINATION_DIR)
+DIR_SPACE_SOURCE=$(dirspace $SOURCE_DIR)
+
+if [ "$DIR_SPACE_SOURCE" -lt "$FREE_DISK_DEST" ]
 then
-    copyall $SOURCE_DIR $PARENT_DEST 
+    copyall $SOURCE_DIR $PARENT_DESTINATION 
 else 
-    echo "Not enough free space no disk. Continue?(Y/N)"
-    read
-    case "$1" in
-    "Y" | "y") 
+    echo 
+    read -p "Not enough free space no disk. Continue?(Y/N)" answer
+    case "$answer" in
+    Y | y) 
         copyall $SOURCE_DIR $PARENT_DEST;;
-    "N" | "n") 
+    N | n) 
         exit 0;;
     *)
         copyfile
@@ -64,11 +65,10 @@ else
 fi
 }
 
-copyfile
-echo $DIRSPACE $FREEDISK
+main () {
+    idenname $SOURCE_DIR $DESTINATION_DIR
+    directoryproblem
+    copyfile
+}
 
-
-
-
-
-MOUNTDISK=$(df /home/ITRANSITION.CORP/e.ilin/Work/Itra/ | tail -1 | awk '{print$1}') #Куда смонтирована
+main
