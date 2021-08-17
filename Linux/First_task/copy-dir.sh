a0ce8db056738674c8b508fa8b237ac9b783106b
Elisey <Jiraiya106@gmail.com>
2021-08-17 16:26:52 +0300

    Linux 3.1

--- Linux/First_task/copy-dir.sh
+++ Linux/First_task/copy-dir.sh
@@ -4,30 +4,30 @@ SOURCE_DIR=$(realpath "$1")
 DESTINATION_DIR=$(realpath "$2")
 
 idenname () {
-if [ "$1" == "$2" ]
-then
-    echo "The same names"
-    exit 0
-fi
+	if [ "$1" == "$2" ]
+	then
+			echo "The same names"
+			exit 0
+	fi
 }
 
 #Free disk
 freedisk () {
-    df "$1" | tail -1 | awk '{print$4}'
+  df "$1" | tail -1 | awk '{print$4}'
 }
 
 directoryproblem () {
-PARENT_SOURCE=$(dirname $SOURCE_DIR)
-PARENT_DESTINATION=$(dirname $DESTINATION_DIR)
+	PARENT_SOURCE=$(dirname "$1")
+	PARENT_DESTINATION=$(dirname "$2")
 
-if [ "$SOURCE_DIR" == "$PARENT_DESTINATION" ]
-then
-    echo ""$DESTINATION_DIR" parent directory "$SOURCE_DIR""
-    exit 0
-elif [ "$DESTINATION_DIR" == "$PARENT_SOURCE" ]
-then 
-    echo ""$SOURCE_DIR" parent directory "$DESTINATION_DIR""
-fi
+	if [ "$1" == "$PARENT_DESTINATION" ]
+	then
+		echo ""$2" parent directory "$1""
+		exit 0
+	elif [ "$2" == "$PARENT_SOURCE" ]
+	then 
+		echo ""$1" parent directory "$2""
+	fi
 }
 
  #Volume directory
@@ -41,32 +41,58 @@ copyall () {
 }
 
 copyfile () {
-FREE_DISK_DEST=$(freedisk $DESTINATION_DIR)
-DIR_SPACE_SOURCE=$(dirspace $SOURCE_DIR)
+	FREE_DISK_DEST=$(freedisk $2)
+	DIR_SPACE_SOURCE=$(dirspace $1)
 
-if [ "$DIR_SPACE_SOURCE" -lt "$FREE_DISK_DEST" ] 
-then
-    copyall $SOURCE_DIR $DESTINATION_DIR 
-else
-    while true; do
-      read -p "Not enough free space no disk. Continue?(Y/N)" answer
-      case "$answer" in
+	if [ "$DIR_SPACE_SOURCE" -lt "$FREE_DISK_DEST" ] 
+	then
+		copyall $1 $2 
+	else
+		while true; do
+			read -p "Not enough no free space disk. Continue?(Y/N)" answer
+			case "$answer" in
 				Y | y) 
-					copyall $SOURCE_DIR $DESTINATION_DIR 
+					copyall $1 $2 
 					break 2;;
 				N | n) 
 					exit 0;;
 				*)
 					echo "Enter Y or N"
-      esac
-    done
-fi
+			esac
+		done
+	fi
 }
 
 main () {
-    idenname $SOURCE_DIR $DESTINATION_DIR
-    directoryproblem
-    copyfile
+    idenname "$SOURCE_DIR" "$DESTINATION_DIR"
+    directoryproblem "$SOURCE_DIR" "$DESTINATION_DIR"
+    #copyfile "$SOURCE_DIR" "$DESTINATION_DIR"
+		copy_arhiv
+		
+}
+
+
+copy_arhiv () {
+	while true;do
+		read -p "We use the date(D) or rotation(R)?" answer
+		case $answer in 
+			D | d) 
+				DATE_NAME=$(date +%Y%m%d_%H%S)
+				tar -czpf $DESTINATION_DIR/$DATE_NAME.tar.gz $SOURCE_DIR
+				break 2;;
+			R | r) 
+				read -p "Maximum number of copies" copies
+				for ((a=$copies; a >= "0"; a--)) do
+					tar -czpf $DESTINATION_DIR/$a.tar.gz $SOURCE_DIR
+					
+					#$copies--
+					echo "$a"
+				done
+				rm $DESTINATION_DIR/$copies.tar.gz
+				break 2;;
+			*) echo "Try again";;
+		esac
+	done
 }
 
 main
