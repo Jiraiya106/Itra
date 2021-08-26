fa3f31aea01347424c7aed3b7496f0bdbc1a0cb0
Elisey <Jiraiya106@gmail.com>
2021-08-26 23:26:45 +0300

    Terraform 1.3

--- Terraform/First_task/main.tf
+++ Terraform/First_task/main.tf
@@ -80,60 +80,41 @@ resource "aws_security_group" "allow_tls_app" {
 }
 
 #RDS
-#resource "aws_db_subnet_group" "my-db" {
-#  name       = "subnet-group"
-#  subnet_ids = [aws_subnet.private_subnet.id, aws_subnet.private_subnet_2.id]
-  
+resource "aws_db_instance" "my-db" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql5.7"
+  vpc_security_group_ids = [ aws_security_group.db_sq.id ]
+  skip_final_snapshot = "true"
 
-#  tags = {
-#    Name = "My DB subnet group"
-#  }
-#}
+  tags = merge({ Name = "${var.common-tags["Name"]}-db"})
+}
 
-#resource "aws_db_instance" "my-db" {
-#  allocated_storage    = 10
-#  engine               = "mysql"
-#  engine_version       = "5.7"
-#  instance_class       = "db.t3.micro"
-#  name                 = "mydb"
-# username             = var.db_username
-#  password             = var.db_password
-#  parameter_group_name = "default.mysql5.7"
-#  db_subnet_group_name = aws_db_subnet_group.my-db.id
-#  vpc_security_group_ids = [ aws_security_group.allow_port_db.id ]
+resource "aws_security_group" "db_sq" {
+  name = "rds_sg"
 
-#  tags = merge({ Name = "${var.common-tags["Name"]}-db"})
-#}
+  dynamic "ingress" {
+  for_each = var.allow_port_db
+    content {
+      from_port        = ingress.value
+      to_port          = ingress.value
+     protocol         = "tcp"
+    cidr_blocks     = ["10.0.1.0/24"]
+    }
+  }
 
-
-#resource "aws_security_group" "allow_port_db" {
-#  name        = "allow_tls_db"
-#  description = "Allow TLS inbound traffic"
-#  vpc_id      = aws_vpc.aws_vpc.id
-
-#  dynamic "ingress" {
-#  for_each = var.allow_port_db
-#    content {
-#      from_port        = ingress.value
-#      to_port          = ingress.value
-#      protocol         = "tcp"
-#      cidr_blocks      = ["0.0.0.0/0"] # Уточнить блоки при создании ELB
-#    }  
-#  }
-
-#  egress  {
-#      from_port        = 0
-#      to_port          = 0
-#      protocol         = "-1"
-#      cidr_blocks      = ["0.0.0.0/0"]
-#      ipv6_cidr_blocks = ["::/0"]
-#    }
-  
-
-#  tags = {
-#    Name = "allow_port_db"
-#  }
-#}
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 #EC2-instance bastion
 
