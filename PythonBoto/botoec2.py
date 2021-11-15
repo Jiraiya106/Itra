3c3bda48b8f9da392f642d54c52b1a94e58d19bb
Elisey <Jiraiya106@gmail.com>
2021-11-15 17:34:38 +0300

    Python 3.3

--- PythonBoto/botoec2.py
+++ PythonBoto/botoec2.py
@@ -11,27 +11,27 @@ def main():
     sg =ec2.describe_security_groups()
     print(sg)
     #pprint(respec2)
-    # try:
-    #     response = ec2.describe_security_groups()
-    #     for i in response['SecurityGroups']:
-    #         print("Security Group ID: "+i['GroupId'])
-    #         print("the Egress rules are as follows: ")
-    #         for j in i['IpPermissionsEgress']:
-    #             #print("IP Protocol: "+j['IpProtocol'])
-    #             for k in j['IpRanges']:
-    #                 print("IP Ranges: "+k['CidrIp'])
-    #         for j in i['IpPermissions']:
-    #             #print("IP Protocol: "+j['IpProtocol'])
-    #             try:
-    #                 print("PORT: "+str(j['FromPort']))
-    #                 for k in j['IpRanges']:
-    #                     print("IP Ranges: "+k['CidrIp'])
-    #             except Exception:
-    #                 print("No value for ports and ip ranges available for this security group")
-    #                 continue
-    #     #print(response[1])
-    # except ClientError as e:
-    #     print(e)
+    try:
+        response = ec2.describe_security_groups()
+        for i in response['SecurityGroups']:
+            print("Security Group ID: "+i['GroupId'])
+            print("the Egress rules are as follows: ")
+            for j in i['IpPermissionsEgress']:
+                #print("IP Protocol: "+j['IpProtocol'])
+                for k in j['IpRanges']:
+                    print("IP Ranges: "+k['CidrIp'])
+            for j in i['IpPermissions']:
+                #print("IP Protocol: "+j['IpProtocol'])
+                try:
+                    print("PORT: "+str(j['FromPort']))
+                    for k in j['IpRanges']:
+                        print("IP Ranges: "+k['CidrIp'])
+                except Exception:
+                    print("No value for ports and ip ranges available for this security group")
+                    continue
+        #print(response[1])
+    except ClientError as e:
+        print(e)
 
     #print(ec2.describe_security_groups())
     #GroupIds=['SECURITY_GROUP_ID']
--- PythonBoto/botofinal.py
+++ PythonBoto/botofinal.py
@@ -2,6 +2,8 @@
 
 import boto3
 from botocore.exceptions import ClientError
+from ipaddress import IPv4Network, IPv4Address
+
 
 client = boto3.client('ec2')
 vpc_id = 'vpc-0824773dc9094b10b'
@@ -9,26 +11,72 @@ vpc_id = 'vpc-0824773dc9094b10b'
 #SG
 def all_sg_CIDR(security):
     try:
-            response = client.describe_security_groups(GroupIds=[security])
-            for i in response['SecurityGroups']:
-                print("Security Group Name: "+i['GroupName'])
-                print("the Egress rules are as follows: ")
-                for j in i['IpPermissionsEgress']:
-                    print("IP Protocol: "+j['IpProtocol'])
+        response = client.describe_security_groups(GroupIds=[security])
+            
+        for i in response['SecurityGroups']:
+            for j in i['IpPermissionsEgress']:
+                for k in j['IpRanges']:
+                    print("IP Ranges Egress: "+k['CidrIp'])
+            for j in i['IpPermissions']:
+                try:
+                    print("PORT: "+str(j['FromPort']))
                     for k in j['IpRanges']:
-                        print("IP Ranges Egress: "+k['CidrIp'])
-                for j in i['IpPermissions']:
-                    print("IP Protocol: "+j['IpProtocol'])
-                    try:
-                        print("PORT: "+str(j['FromPort']))
-                        for k in j['IpRanges']:
-                            print("IP Ranges Ingress: "+k['CidrIp'])
-                    except Exception:
-                        print("No value for ports and ip ranges available for this security group")
-                        continue
+                        print("IP Ranges Ingress: "+k['CidrIp'])
+                            
+                except Exception:
+                    print("No value for ports and ip ranges available for this security group")
+                    continue
     except ClientError as e:
         print(e)
 
+def list_egreess(security):
+    m = []
+    response = client.describe_security_groups(GroupIds=[security])
+    for i in response['SecurityGroups']:
+        for j in i['IpPermissionsEgress']:
+            for k in j['IpRanges']:
+                m.append(k['CidrIp'])
+
+def list_ingress(security):
+    response = client.describe_security_groups(GroupIds=[security])
+    for i in response['SecurityGroups']:
+        for j in i['IpPermissions']:
+            try:
+                print("PORT: "+str(j['FromPort']))
+                for k in j['IpRanges']:
+                    print("IP Ranges Ingress: "+k['CidrIp'])
+            except Exception:
+                print("No value for ports and ip ranges available for this security group")
+                continue
+
+def list_ingress_port(security):
+    m = []
+    response = client.describe_security_groups(GroupIds=[security])
+    for i in response['SecurityGroups']:
+        for j in i['IpPermissions']:
+                m.append(j['FromPort'])
+    return m
+
+def list_ingress_cidr(security):
+    response = client.describe_security_groups(GroupIds=[security])
+    m = []
+    for i in response['SecurityGroups']:
+        for j in i['IpPermissions']:
+            try:
+                for k in j['IpRanges']:
+                    m.append(k['CidrIp'])
+            except Exception:
+                print("No value for ports and ip ranges available for this security group")
+                continue
+    return m
+
+
+def list_true_false(a, b, c):
+    for i in range(len(a)):
+        k = IPv4Network(a[i])
+        for l in range(len(b)):
+            m = IPv4Address(b[l]) in k
+            print('Instance ' + c[l] + ' ' + 'in ingress ' + a[i] + ': ' + str(m))
 #EC2
 def list_all_ec2_instances(client, vpc_id):
     response = client.describe_instances(Filters = [{'Name': 'vpc-id', 'Values': [vpc_id]}])
@@ -63,11 +111,13 @@ def list_rds_name(rds_instances):
 def main():
     for i in range(len(id_ec2)):
         print( 'EC2 Instances: ' +  id_ec2[i])
-        print( all_sg_CIDR( ec2_sg[i]) )
+        list_egreess( ec2_sg[i])
         print( 'Private address EC2: ' + ec2_address[i] + '\n')
     for i in range(len(rds)):
         print( 'RDS Instance: ' + rds[i] )
-        print( all_sg_CIDR( rds_sg[i] ) )
+        list_ingress( rds_sg[i] )
+        #print( list_ingress_cidr( rds_sg[i] ) )
+        list_true_false( list_ingress_cidr( rds_sg[i] ), ec2_address, id_ec2 ) 
         print( " " )
 
 rds_instances = list_all_rds_instances(vpc_id)
@@ -80,5 +130,6 @@ id_ec2 = list_ec2_id(ec2_instances)
 ec2_sg = list_ec2_sec_group_ids(ec2_instances)
 ec2_address = list_private_address_ec2(ec2_instances)
 
-#print( ec2_address )
+#print( list_ingress_cidr( 'sg-0cd275ab14229c79d' ) )
+#all_sg_CIDR( 'sg-0cd275ab14229c79d' )
 main()
